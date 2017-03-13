cf23a699a4683eb67b4d42a6b8ce98bf6ab75e28
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2017-03-13 16:44:32 +1100

    Update news list display after every rendering

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -118,6 +118,11 @@ class News extends Component {
 
   }
 
+  componentDidUpdate() {
+    // This is being used to scroll to the top of the news list after every rendering
+    document.querySelector( ".news-abstract-list" ).scrollTop = 0;
+  }
+
   render() {
     const unsplashStyle = {
       backgroundImage: 'url( "https://source.unsplash.com/random" )'
